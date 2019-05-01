5e22b37fbaec10e647cffeec0a365a35083c8d92
kbh0581 <kbh0581@gmail.com>
2019-05-01 17:36:52 +0900

    최종 완료

--- main.c
+++ main.c
@@ -37,12 +37,7 @@ void draw_kbh(void);                        /* 배열의 크기를 입력 받아
 int main(int argc, const char * argv[]) {
     
     change_kbh(); //1번 함수 호출
-    
-    drawSandglass(5);
     draw_kbh();   //2번 함수 호출
-    
-    
-    printf("Hello, World!\n");
     return 0;
 }
 
@@ -87,24 +82,42 @@ void swapArray(char input[],int size){
     }
 }
 
-/*   1            13 */         // 0,0 0,1 0,2 0,3 0,4 // 1,1 1,2 1,3 // 2,2 // 3,1 3,2 ,3,3 // 4,0 // 4,1 // 4,2// 4,3 4,4
+/*   1            13 */
 /*   2  6     10  14 */
 /*   3  7  9  11  15 */
 /*   4  8     12  16 */
 /*   5            17  그리는 함수*/
 void drawSandglass(int num){
-    int index = 1;
-    int sandArray[num][num];
+    int index = 1;  //배열의 숫자의 값
+    int sandArray[num][num];    //그려줄 배열 선언
+    
+    for (int row = 0; row < num; row++){    //2차원 배열 0으로 초기화
+        for(int cal = 0; cal<num; cal++) {
+            sandArray[row][cal] = 0;
+        }
+     }
+    
     for (int row = 0; row < num; row++){
-        for(int cal = row; cal<num + (-2 * row); cal++) {
-            sandArray[row][cal] = index++;
+        if(row<=num/2){                                 //가운대 열을 기준으로 왼쪽인지 오른쪽인지 판단
+            for(int cal = row; cal<num - row; cal++) {  //왼쪽 열일 경우 행의 값을 찍어줌 찍어주는 범위는 2씩 줄어들고 시작점은 하나씩 줄어듬
+                sandArray[row][cal] = index++;
+            }
+        }else{
+            for(int cal = num-row-1; cal<=row; cal++) { //오른쪽 열일 경우 행의 값을 찍어줌 찍어주는 범위는 2씩늘어나고 시작점은 하나씩 늘어남
+                sandArray[row][cal] = index++;
+            }
         }
     }
     
-    //출력
+    //최종 결과를 찍어주는 반복문
     for (int i = 0; i < num; i++) {
         for (int j =0 ; j<num; j++) {
-            printf("%d",sandArray[i][j]);
+            if(sandArray[j][i] != 0){
+                printf("%d\t",sandArray[j][i]);     //값이 0이 아닌경우 숫자 출력
+            }else{
+                printf(" \t");                      //값이 0인 인경우 공백 문자 출력
+            }
+            
         }
         printf("\n");
     }
@@ -152,16 +165,17 @@ void draw_kbh(){
     int num;
     printf("5부터 10사이의 홀수 숫자를 입력하시오\n (0을 입력시 프로그램 종료 \n");
     printf(" 숫자 입력 : ");
-    scanf("%n",&num);
+    scanf("%d",&num);
     clearBuffer();
     
+    
     while (1) {
-        if(num==EXIT_FLAG){     //0 일경우 출력
+        if(num==EXIT_FLAG){     //0 일경우 출력후 프로그램 종료
             printf("-----------------------------\n");
             printf("♥프로그램을 종료합니다. 이름 : 김병희 \n");
             printf("-----------------------------\n");
             return;               // 함수 끝
-        }else if(num>LIMIT_NUM || num < LIMIT_NUM){     //5~10의 값이 아닐경우 제한
+        }else if(num < LOWER_NUM || num > LIMIT_NUM){     //5~10의 값이 아닐경우 제한
             printf("--------------------------------------------- \n");
             printf(" ☆ 5부터 10 사이의 홀수를 입력하시오. 학과 : 컴퓨터과학과 \n");
             printf("--------------------------------------------- \n");
@@ -173,8 +187,9 @@ void draw_kbh(){
             drawSandglass(num);         //모래시계 그리는 함수
         }
         printf(" 숫자 입력 : ");
-        scanf("%n",&num);
+        scanf("%d",&num);
         clearBuffer();
+        
     
     }
 }
